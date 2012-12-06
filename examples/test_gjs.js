895ff2552d8c4da439e50c52d5f3b355e92e3b92
Yaacov Zamir <kobi.zamir@gmail.com>
2012-12-06 09:41:59 +0200

    libhdate is using  owned strings since version 1.6

--- examples/test_gjs.js
+++ examples/test_gjs.js
@@ -1,7 +1,15 @@
 #!/usr/bin/gjs
 
 const Hdate = imports.gi.LibHdateGlib.Hdate;
-var h = Hdate.new();
 
-h.set_gdate(2,12,2012)
+var h = Hdate.new();
+h.set_gdate(0, 0, 0);
+
 print (h.get_format_date(0,0));
+
+h.set_gdate(1, 1, 2013);
+
+print (h.get_format_date(0,0));
+
+print (h.get_string(0, 5773, false, true));
+
